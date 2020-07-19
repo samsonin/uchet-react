db1f72e8c51a0d774b5f28717a7c4bbaa35f4edb
Ivan <samsonin@mail.ru>
2020-07-19 18:19:42 +1000

    upd

--- src/components/Fields.js
+++ src/components/Fields.js
@@ -2,14 +2,12 @@ import {bindActionCreators} from "redux";
 import {closeSnackbar, enqueueSnackbar, upd_app} from "../actions/actionCreator";
 import {connect} from "react-redux";
 import React, {Component, Fragment} from "react";
-import request from "./Request";
 import FormControl from "@material-ui/core/FormControl";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import IconButton from "@material-ui/core/IconButton";
 import Input from "@material-ui/core/Input";
 import DeleteIcon from '@material-ui/icons/Delete';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import SaveIcon from '@material-ui/icons/Save';
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
 import Typography from "@material-ui/core/Typography";
 import {Button} from "@material-ui/core";
@@ -35,17 +33,23 @@ export default connect(state => (state), mapDispatchToProps)(class extends Compo
 
     state = {
         index: 'customer',
+        systemFieldsHandle: 0,
+        customerFieldsCounter: 0
     }
 
     componentDidMount() {
 
-        let state = []
+        let fields = []
         this.props.app.fields.allElements
             .filter(field => field.index === this.state.index)
             .map(v => {
-                state.push(v);
+                fields.push(v);
             })
-        this.setState({fields: state})
+        this.setState({
+            fields,
+            systemFieldsHandle: 0,
+            customerFieldsCounter: 0
+        })
 
     }
 
@@ -57,13 +61,76 @@ export default connect(state => (state), mapDispatchToProps)(class extends Compo
         })
     }
 
+    systemFieldsHandle(systemFieldsHandle) {
+        this.setState({systemFieldsHandle})
+    }
+
+    addField() {
+
+        let fields = this.state.fields
+        if (this.state.systemFieldsHandle === 0) {
+
+            let cf = ++this.state.customerFieldsCounter;
+            fields.splice(0, 0, {
+                index: this.state.index,
+                name: 'cf' + cf,
+                value: '',
+                is_system: false,
+                is_valid: true,
+            })
+            this.setState({
+                fields,
+                customerFieldsCounter: cf
+            })
+
+        } else {
+
+            let fields = this.state.fields.map(f => {
+                if (f.name === this.state.systemFieldsHandle) f.is_valid = true;
+                return f;
+            })
+            this.setState({
+                fields,
+                systemFieldsHandle: 0
+            })
+
+        }
+    }
+
     fieldHandle(name, value) {
 
         let fields = this.state.fields
         fields.find(field => field.name === name).value = value
         this.setState({fields})
 
-        console.log(fields)
+    }
+
+    moveField(name, direction) {
+
+        let field = this.state.fields.filter(f => f.name === name)[0]
+        let i = this.state.fields.indexOf(field)
+        let fields = this.state.fields.filter(f => f.name !== name)
+
+        if (direction === 'up') i--
+        if (direction === 'down') i++
+        if (i === -1) i = 0
+
+        fields.splice(i, 0, field)
+
+        this.setState({fields})
+
+    }
+
+    deleteField(field) {
+
+        let fields = field.is_system ?
+            this.state.fields.map(f => {
+                if (f.name === field.name) f.is_valid = false;
+                return f;
+            }) :
+            this.state.fields.filter(f => f.name !== field.name);
+
+        this.setState({fields})
 
     }
 
@@ -79,49 +146,54 @@ export default connect(state => (state), mapDispatchToProps)(class extends Compo
                 >
 
                     <Select
-                        variant="outlined"
                         style={{width: '75%'}}
-                        value={this.state.index}
+                        value={index}
                         onChange={e => this.indexHandle(e.target.value)}
                     >
                         {['order', 'customer'].map(i => <MenuItem value={i} key={"fieldindexmenuuywgvf" + i}>
-                                {this.props.app.fields.alliases[i]}
+                                <Typography variant="h5">
+                                    {this.props.app.fields.alliases[i]}
+                                </Typography>
                             </MenuItem>
                         )}
                     </Select>
 
+                </Grid>
+
+                <Grid container direction="row" justify="space-evenly"
+                      style={{marginBottom: '1rem'}}
+                >
+                    <FormControl style={{width: '75%'}}>
+                        <Select
+                            variant="outlined"
+                            value={this.state.systemFieldsHandle}
+                            onChange={e => this.systemFieldsHandle(e.target.value)}
+                        >
+                            <MenuItem value="0" key={"addsysmfield"}>
+                                Новое поле
+                            </MenuItem>
+                            {this.state.fields.map(v => v.is_system && !v.is_valid ?
+                                <MenuItem value={v.name} key={"addsysmfield" + v.name}>
+                                    {v.value}
+                                </MenuItem> : ''
+                            )}
+                        </Select>
+                    </FormControl>
+
                     <Tooltip title="Добавить">
-                        <IconButton>
+                        <IconButton onClick={() => this.addField()}>
                             <AddCircleIcon/>
                         </IconButton>
                     </Tooltip>
+
                 </Grid>
 
-                {/*{this.state !== null && this.state.newFields[index] === '' ?*/}
-                {/*    <FormControl variant="outlined" className="m-2 w-25" key={"addsysmfcw4g45" + index}>*/}
-                {/*        <InputLabel>*/}
-                {/*            Системные поля*/}
-                {/*        </InputLabel>*/}
-                {/*        <Select*/}
-                {/*            value=""*/}
-                {/*            onChange={(e) => this.changeForm(index, e.target.value, 'setValid')}*/}
-                {/*            className="m-2 w-100"*/}
-                {/*            autoWidth*/}
-                {/*        >*/}
-                {/*            /!*{this.props.app.fields.allElements[index].map((v) => v.isSystem && !v.isValid ?*!/*/}
-                {/*            /!*    <MenuItem value={v.name} key={"addsysmfield" + v.name}>*!/*/}
-                {/*            /!*        {v.value}*!/*/}
-                {/*            /!*    </MenuItem> : ''*!/*/}
-                {/*            /!*)}*!/*/}
-                {/*        </Select>*/}
-                {/*    </FormControl>*/}
-                {/*    : ''}*/}
-
-                {this.state.fields.map(field => <FormControl key={"elem" + index + field.name}
-                                                             style={{
-                                                                 width: '100%',
-                                                                 padding: '1rem'
-                                                             }}>
+                {this.state.fields.map(field => field.is_valid ?
+                    <FormControl key={"elem" + index + field.name}
+                                 style={{
+                                     width: '100%',
+                                     padding: '1rem'
+                                 }}>
                         <Input
                             value={field.value}
                             disabled={field.is_system}
@@ -129,30 +201,39 @@ export default connect(state => (state), mapDispatchToProps)(class extends Compo
                             endAdornment={
                                 <InputAdornment position="end">
                                     <IconButton
-                                        // onClick={() => this.changeForm(index, field.name, 'down')}
+                                        onClick={() => this.moveField(field.name, 'down')}
                                     >
                                         {<ArrowDownwardIcon/>}
                                     </IconButton>
                                     <IconButton
-                                        // onClick={() => this.changeForm(index, field.name, 'up')}
+                                        onClick={() => this.moveField(field.name, 'up')}
                                     >
                                         {< ArrowUpwardIcon/>}
                                     </IconButton>
                                     <IconButton
-                                        onClick={() => this.changeForm(index, field.name, field.value)}
+                                        onClick={() => this.deleteField(field)}
                                     >
                                         {<DeleteIcon/>}
                                     </IconButton>
                                 </InputAdornment>
                             }
                         />
-                    </FormControl>
+                    </FormControl> :
+                    ''
                 )}
 
                 <Grid container justify="flex-end">
                     <Button
+                        variant="contained"
+                        color="secondary"
+                        style={{margin: '1rem'}}
+                        onClick={() => this.componentDidMount()}
+                    >
+                        Отмена
+                    </Button>
+                    <Button
                         disabled={JSON.stringify(this.state.fields) === JSON.stringify(this.props.app.fields.allElements
-                                .filter(field => field.index === index))
+                            .filter(field => field.index === index))
                         }
                         variant="contained"
                         color="primary"
