d71bee0f75d3590a76cead78ebccacf68ab62556
ivan <samsonin@mail.ru>
2020-08-09 09:23:46 +1000

    upd

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import TextField from "@material-ui/core/TextField/TextField";
 
-export default function Field(props) {
+export default function (props) {
 
     return <TextField
         style={{
--- src/components/Rest.js
+++ src/components/Rest.js
@@ -1,15 +1,4 @@
-const isLocalhost = Boolean(
-    window.location.hostname === "localhost" ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === "[::1]" ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
-const SERVER = isLocalhost ?
-    'http://127.0.0.1:8000' :
-    'https://api.uchet.store';
+import {SERVER} from '../constants';
 
 let response = {};
 
