ada52884d8860b887426a08273f83f3725797fe5
Ivan <samsonin@mail.ru>
2020-08-10 12:32:00 +1000

    upd

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+
+import IconButton from "@material-ui/core/IconButton";
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 
 import {SERVER} from '../constants';
 
+
 export const Player = ({recordingId}) => {
+
+  const [loaded, setLoaded] = useState(false)
 
   let jwt = JSON.parse(window.localStorage.getItem('auth')).jwt;
   if (typeof jwt !== "string") return false;
 
-  return <>
-    <audio
-      src={SERVER + '/records/' + recordingId}
+  return loaded
+    ? <audio
+      src={SERVER + '/records/' + recordingId + '?jwt=' + jwt}
       controls
+      // crossOrigin="use-credentials"
     />
-    <audio
-      src={SERVER + '/records/' + recordingId}
-      controls
-      crossOrigin="use-credentials"
-    />
-  </>
+    : <IconButton onClick={() => setLoaded(true)}>
+      <PlayArrowIcon/>
+    </IconButton>
 }
