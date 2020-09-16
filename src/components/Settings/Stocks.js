ae83a433b0de35b644ca0198f35881762ecdcea5
samsonin <samsonin@mail.ru>
2020-09-16 17:53:30 +1000

    upd

--- src/components/Settings/Stocks.js
+++ src/components/Settings/Stocks.js
@@ -45,90 +45,90 @@ function Stocks(props) {
         }
     }
 
-    const renderFab = () => <Fab color="primary" aria-label="add" className="addfab" onClick={add}>
-        <AddIcon/>
-    </Fab>
-
     const requestSettings = (id, index, value) => {
 
         if (value === '') return false;
 
         if (!isRequest) {
             setIsRequest(true);
-            rest({
-                action: 'changePoint',
-                id,
-                index,
-                value
-            }, '/settings', props.auth.jwt)
+            rest('stocks/' + id, 'PATCH', {
+                [index]: value
+            })
                 .then(data => {
 
                     setIsRequest(false);
+
                     if (data.result) {
 
                         const {upd_app} = props;
                         upd_app({stocks: data.stocks})
 
                     } else {
-                        let message = 'ошибка';
-                        if (data.error === 'already_used') message = 'Такой пользователь уже зарегистрирован!';
-                        else if (data.error === 'wrong_format') message = 'Неправильный формат контакта!';
-                        enqueueSnackbar(message, {
-                            variant: 'warning'
-                        })
+
+                        // let message = 'ошибка';
+                        // if (data.error === 'already_used') message = 'Такой пользователь уже зарегистрирован!';
+                        // else if (data.error === 'wrong_format') message = 'Неправильный формат контакта!';
+                        // enqueueSnackbar(message, {
+                        //     variant: 'warning'
+                        // })
+
                     }
+
                 })
         }
     };
 
-    return props.app.stocks.map(v => {
-        return <div key={"grKeydiv" + v.id}>
-            <Grid container direction="row" className="hoverable m-2 p-3" key={"grKey" + v.id}>
-                <Grid item xs={9}>
-                    <Typography variant="h3">
-                        <i className="fas fa-store"/>
-                    </Typography>
+    return <>
+        {props.app.stocks.map(v => <div key={"grKeydiv" + v.id}>
+                <Grid container direction="row" className="hoverable m-2 p-3" key={"grKey" + v.id}>
+                    <Grid item xs={9}>
+                        <Typography variant="h3">
+                            <i className="fas fa-store"/>
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={3}>
+                        <FormControlLabel
+                            label={v.is_valid ? 'Активна' : 'Не активна'}
+                            control={
+                                <Switch checked={!!v.is_valid}
+                                        onChange={e => requestSettings(v.id, 'is_valid', e.target.checked)}
+                                        color="primary"/>
+                            }
+                        />
+                        {/*{!v.is_valid && v.canDelete ?*/}
+                        {/*    <IconButton color="secondary" onClick={() => this.deletePoint(v.id)}>*/}
+                        {/*        <DeleteIcon/>*/}
+                        {/*    </IconButton>*/}
+                        {/*: ''}*/}
+                    </Grid>
+                    <Grid item xs={12}>
+                        <InputLabel className="mt-2 font-weight-bold">Название:</InputLabel>
+                        <TextField
+                            defaultValue={v.name} className="w-75"
+                            onBlur={e => requestSettings(v.id, 'name', e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <InputLabel className="mt-2 font-weight-bold">Адрес:</InputLabel>
+                        <TextField
+                            defaultValue={v.address} className="w-75"
+                            onBlur={(e) => requestSettings(v.id, 'address', e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <InputLabel className="mt-2 font-weight-bold">Телефон:</InputLabel>
+                        <TextField
+                            defaultValue={v.phone_number} className="w-75"
+                            onBlur={e => requestSettings(v.id, 'phone_number', e.target.value)}
+                        />
+                    </Grid>
                 </Grid>
-                <Grid item xs={3}>
-                    <FormControlLabel
-                        label={v.is_valid ? 'Активна' : 'Не активна'}
-                        control={
-                            <Switch checked={!!v.is_valid}
-                                    onChange={e => requestSettings(v.id, 'is_valid', e.target.checked)}
-                                    color="primary"/>
-                        }
-                    />
-                    {/*{!v.is_valid && v.canDelete ?*/}
-                    {/*    <IconButton color="secondary" onClick={() => this.deletePoint(v.id)}>*/}
-                    {/*        <DeleteIcon/>*/}
-                    {/*    </IconButton>*/}
-                    {/*: ''}*/}
-                </Grid>
-                <Grid item xs={12}>
-                    <InputLabel className="mt-2 font-weight-bold">Название:</InputLabel>
-                    <TextField
-                        defaultValue={v.name} className="w-75"
-                        onBlur={e => requestSettings(v.id, 'name', e.target.value)}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <InputLabel className="mt-2 font-weight-bold">Адрес:</InputLabel>
-                    <TextField
-                        defaultValue={v.address} className="w-75"
-                        onBlur={(e) => requestSettings(v.id, 'address', e.target.value)}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <InputLabel className="mt-2 font-weight-bold">Телефон:</InputLabel>
-                    <TextField
-                        defaultValue={v.phone_number} className="w-75"
-                        onBlur={e => requestSettings(v.id, 'phone_number', e.target.value)}
-                    />
-                </Grid>
-            </Grid>
-            {renderFab()}
-        </div>
-    })
+            </div>
+        )}
+        <Fab color="primary" aria-label="add" className="addfab" onClick={add}>
+            <AddIcon/>
+        </Fab>
+    </>
 
 }
 
