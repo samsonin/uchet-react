83d59c30e4f5f0890e3f01bc9d9769ec21312235
samsonin <samsonin@mail.ru>
2021-05-05 20:11:40 +1000

    remove allowed from queue

--- src/components/Queue.js
+++ src/components/Queue.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {connect} from "react-redux";
 import rest from "./Rest";
 
@@ -13,12 +13,8 @@ import {Select} from "@material-ui/core";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Typography from "@material-ui/core/Typography";
-import {enqueueSnackbar, upd_app} from "../actions/actionCreator";
-import {bindActionCreators} from "redux";
 import Grid from "@material-ui/core/Grid";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
+import {useSnackbar} from "notistack";
 
 const MyPaper = styled(Paper)({
     marginTop: 20,
@@ -32,57 +28,50 @@ const MyFormControl = styled(FormControl)({
     minWidth: 150,
 });
 
-class Queue extends React.Component {
+const AvailableStock = (props) => {
 
-    state = {
-        allowed: false
-    }
+    const {enqueueSnackbar} = useSnackbar();
 
-    allowedChange = () => {
-        this.setState({allowed: !this.state.allowed})
-    }
+    const pointChange = (user_id, stock_id, permission) => {
 
-    pointChange = (user_id, stock_id, permission) => {
+        console.log(user_id, stock_id, permission)
 
         rest('queue', 'PATCH',
             {user_id, stock_id, permission})
             .then(res => {
 
                 if (res.ok) {
-                    const {upd_app} = this.props;
-                    upd_app(res.body);
+                    // const {upd_app} = props;
+                    // upd_app(res.body);
                 }
 
-                this.props.enqueueSnackbar({
-                    message: res.ok ? 'ok' : res.error,
-                    options: {
-                        variant: res.ok ? 'success' : 'warning',
-                    },
-                });
+                enqueueSnackbar(res.ok ? 'ok' : res.error,
+                    {variant: res.ok ? 'success' : 'warning'}
+                    );
 
             })
 
     };
 
-    renderTable(o, permission) {
+    const renderTable = (o, permission) => {
 
         let arr = [];
         for (const user_id in o) {
             arr.push(
                 <TableRow key={'rowkey' + user_id + o[user_id]}>
                     <TableCell>
-                        {this.props.app.users.find(v => +v.id === +user_id).name}
+                        {props.app.users.find(v => +v.id === +user_id).name}
                     </TableCell>
                     <TableCell>
                         <MyFormControl variant="outlined">
-                            <Select onChange={e => this.pointChange(+user_id, +e.target.value, permission)}
+                            <Select onChange={e => pointChange(+user_id, +e.target.value, permission)}
                                     value={o[user_id]}
                             >
                                 <MenuItem key={'mik0'} value={0}>
                                     <br/>
                                 </MenuItem>
 
-                                {this.props.app.stocks.map(value => value.is_valid ?
+                                {props.app.stocks.map(value => value.is_valid ?
                                     <MenuItem
                                         key={'mik' + value.id}
                                         value={value.id}
@@ -94,20 +83,6 @@ class Queue extends React.Component {
                             </Select>
                         </MyFormControl>
                     </TableCell>
-                    <TableCell>
-                        { this.state.allowed ? <FormGroup>
-                            {this.props.app.stocks.map(v => {
-                            return v.is_valid ? <FormControlLabel
-                                control={<Checkbox
-                                    color="primary"
-                                    checked={true}
-                                    // onChange={handleChange}
-                                />}
-                                label={v.name}
-                            /> : ''
-                            })}
-                        </FormGroup> : '' }
-                    </TableCell>
                 </TableRow>
             )
 
@@ -116,67 +91,50 @@ class Queue extends React.Component {
 
     }
 
-    render() {
-        return this.props.app.queue ?
-            <Grid container spacing={3} alignContent="center">
+    return props.app.queue
 
+        ? <Grid container spacing={3} alignContent="center">
+
+            <Grid item>
+                <Typography variant="h5">Расстановка мастеров</Typography>
+                <MyPaper>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Мастер</TableCell>
+                                <TableCell>Куда вышел на смену</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {renderTable(props.app.queue, 'masters')}
+                        </TableBody>
+                    </Table>
+                </MyPaper>
+            </Grid>
+
+            {props.auth.admin && props.app.salersQueue ?
                 <Grid item>
-                    <Typography variant="h5">Расстановка мастеров</Typography>
+                    <Typography variant="h5">Расстановка администраторов</Typography>
                     <MyPaper>
                         <Table>
                             <TableHead>
                                 <TableRow>
-                                    <TableCell>Мастер</TableCell>
+                                    <TableCell>Администратор</TableCell>
                                     <TableCell>Куда вышел на смену</TableCell>
-                                    { this.props.auth.admin ?
-                                        <TableCell>
-                                            <Checkbox
-                                                color="primary"
-                                                checked={this.state.allowed}
-                                                onChange={this.allowedChange}
-                                            />
-                                            Разрешенные точки
-                                        </TableCell>
-                                        : ''
-                                    }
                                 </TableRow>
                             </TableHead>
                             <TableBody>
-                                {this.renderTable(this.props.app.queue, 'masters')}
+                                {renderTable(props.app.salersQueue, 'sellers')}
                             </TableBody>
                         </Table>
                     </MyPaper>
                 </Grid>
+                : null
+            }
 
-                {this.props.auth.admin && this.props.app.salersQueue ?
-                    <Grid item>
-                        <Typography variant="h5">Расстановка администраторов</Typography>
-                        <MyPaper>
-                            <Table>
-                                <TableHead>
-                                    <TableRow>
-                                        <TableCell>Администратор</TableCell>
-                                        <TableCell>Куда вышел на смену</TableCell>
-                                    </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                    {this.renderTable(this.props.app.salersQueue, 'sellers')}
-                                </TableBody>
-                            </Table>
-                        </MyPaper>
-                    </Grid>
-                    : ''
-                }
-
-            </Grid> :
-            'Загружаем данные...'
-    }
+        </Grid>
+        : 'Загружаем данные...'
 
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-    enqueueSnackbar,
-    upd_app
-}, dispatch);
-
-export default connect(state => (state), mapDispatchToProps)(Queue);
+export default connect(state => state)(AvailableStock);
