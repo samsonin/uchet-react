7140c294e27bf43b5303dbf748bca5195e9980fc
samsonin <samsonin@mail.ru>
2022-07-27 19:15:40 +1000

    add PrintBarcodes

--- src/components/common/PrintBarcodes.js
+++ src/components/common/PrintBarcodes.js
@@ -1,36 +1,34 @@
 import JsBarcode from 'jsbarcode';
 
-export function PrintBarcodes(barcodes, afterPrint) {
+const pStyle = "body {margin: 0, padding: 0} @page { size: 30mm 20mm; margin: 0; padding: 0} svg { overflow: hidden; position: relative; width: 28mm; height: 16mm; margin: 0; padding: 0; page-break-after: always; }"
 
-    console.log(barcodes)
+export function PrintBarcodes(barcodes) {
 
-    const printable = document.getElementsByClassName('printable')
-
-    for (let i = 0; i < printable.length; i++) {
-        printable[i].remove()
-    }
+    const newWin = window.open()
+    const style = document.createElement('style')
+    style.innerText = pStyle
+    newWin.document.head.append(style)
 
     const div = document.createElement('div')
-    div.className = 'printable'
 
     barcodes.map(b => {
 
-        const svg = document.createElement('svg')
-        svg.className = "barcode"
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+        svg.setAttribute("class", "barcode")
         svg.setAttribute("jsbarcode-value", b)
+        svg.setAttribute("jsbarcode-format", "EAN13")
+        svg.setAttribute("jsbarcode-displayvalue", "false")
         div.appendChild(svg)
 
     })
 
     document.body.append(div)
 
-    JsBarcode(".barcode").init();
+    JsBarcode(".barcode").init()
 
-    if (typeof (afterPrint) === "function") window.onafterprint = () => afterPrint()
-
-    window.print()
-
-    window.onafterprint = null
+    newWin.document.body.append(div)
+    newWin.onafterprint = newWin.close
+    newWin.print()
 
     div.remove()
 
