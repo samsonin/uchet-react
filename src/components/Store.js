b9e8a0fe8dcef447e53588638285763ed9d75e2e
samsonin <samsonin@mail.ru>
2022-08-26 20:49:12 +1000

    add rest in Store

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from "react";
 import {connect} from "react-redux";
-import {Button, IconButton} from "@material-ui/core";
+import {Button, IconButton, InputAdornment, Table, TableBody, TableCell, TableRow, TextField} from "@material-ui/core";
 import uuid from "uuid";
 import Tree from "./Tree";
 import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
 import rest from "../components/Rest";
+import TableHead from "@material-ui/core/TableHead";
+import TwoLineInCell from "./common/TwoLineInCell";
+import SearchIcon from "@material-ui/icons/Search";
 
 
 const oftenUsedButtons = [
@@ -15,20 +18,31 @@ const oftenUsedButtons = [
     {label: 'Расходники', catId: 999},
 ]
 
+const style = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    margin: '.5rem',
+}
+
 const Store = props => {
 
+    const [goods, setGoods] = useState([])
     const [categoryFilter, setCategoryFilter] = useState(0)
     const [isExpand, setIsExpand] = useState(false)
-    const [count, setCount] = useState(25)
+    const [search, setSearch] = useState('')
+    const [limit, setLimit] = useState(25)
 
-    useEffect(() => {
+
+    const sendRequest = () => {
+
+        if (!categoryFilter) return
 
         rest('goods?category_id=' + categoryFilter)
             .then(res => {
-
+                if (res.status === 200) setGoods(res.body)
             })
 
-    }, [])
+    }
 
 
     const setCat = id => {
@@ -59,29 +73,109 @@ const Store = props => {
 
     }
 
-    return <div>
+    return <>
 
-        {isExpand
-            ? <Tree categories={props.app.categories}
-                    onSelected={id => setCat(id)}
-                    finished={id => setCat(id)}
+        <div className="w-100">
+
+            {isExpand
+                ? <Tree categories={props.app.categories}
+                          onSelected={id => setCat(id)}
+                          finished={id => setCat(id)}
+                    />
+                : <IconButton onClick={() => setIsExpand(!isExpand)}>
+                    <ExpandLessIcon/>
+                </IconButton>}
+
+            {oftenUsedButtons.map(b => <Button
+                key={uuid()}
+                size="small"
+                style={{margin: '.5rem'}}
+                color={categoryFilter === b.catId ? "primary" : "default"}
+                variant={categoryFilter === b.catId ? "contained" : "outlined"}
+                onClick={() => setCategoryFilter(b.catId)}>
+                {b.label}
+            </Button>)}
+
+        </div>
+
+        <div style={style}>
+
+            <TextField
+                       InputProps={{
+                           startAdornment: (
+                               <InputAdornment position="start">
+                                   <SearchIcon/>
+                               </InputAdornment>
+                           ),
+                       }}
+                       value={search}
+                       onChange={e => setSearch(e.target.value)}
             />
-            : <IconButton onClick={() => setIsExpand(!isExpand)}>
-                <ExpandLessIcon/>
-            </IconButton>}
 
-        {oftenUsedButtons.map(b => <Button
-            key={uuid()}
-            size="small"
-            style={{margin: '.5rem'}}
-            color={categoryFilter === b.catId ? "primary" : "default"}
-            variant={categoryFilter === b.catId ? "contained" : "outlined"}
-            onClick={() => setCategoryFilter(b.catId)}>
-            {b.label}
-        </Button>)}
+            <Button onClick={() => sendRequest()}>
+                Найти
+            </Button>
+
+        </div>
+
+        {goods.length
+            ? <Table size="small"
+                     style={{background: 'white'}}
+            >
+                <TableHead>
+                    <TableRow>
+                        <TableCell>#</TableCell>
+                        <TableCell>Группа</TableCell>
+                        <TableCell>Наименование / imei</TableCell>
+                        <TableCell>Цена / Себестоимость</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {goods
+                        .filter(s => {
+
+                            if (!search || s.sum == search || s.id == search) return true
+
+                            const model = s.model.toLowerCase()
+                            const imei = s.imei.toLowerCase()
+
+                            let r = true
+
+                            search.toLowerCase()
+                                .split(' ')
+                                .map(s => {
+
+                                    if (model.indexOf(s) < 0 && imei.indexOf(s) < 0) {
+                                        r = false
+                                    }
+
+                                })
+
+                            return r
 
 
-    </div>
+                        })
+                        .map(g => <TableRow key={'tablerowingoods' + g.id}
+                                            style={{
+                                                cursor: 'pointer',
+                                                opacity: props.app.stock_id === g.stock_id ? '100%' : '50%',
+                                            }}
+                                            onClick={() => console.log(g)}
+                        >
+                            <TableCell>{g.id}</TableCell>
+                            <TableCell>{g.group}</TableCell>
+                            <TableCell>
+                                {TwoLineInCell(g.model, g.imei)}
+                            </TableCell>
+                            <TableCell>
+                                {g.sum}
+                            </TableCell>
+                        </TableRow>)}
+                </TableBody>
+            </Table>
+            : 'Нет данных'}
+
+    </>
 
 }
 
